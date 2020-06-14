30635688d6855ba9b33c4dbccfb4f607e4a0ffa5
Satish Chandan <53550303+SatishC16@users.noreply.github.com>
2020-06-14 13:00:44 +0530

    Create 2.cpp

---
+++ Day1(Array)/2.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sort012(int a[],int n)
+{
+    int lo = 0; 
+    int hi = n - 1; 
+    int mid = 0; 
+  
+    // Iterate till all the elements 
+    // are sorted 
+    while (mid <= hi) { 
+        switch (a[mid]) { 
+  
+        // If the element is 0 
+        case 0: 
+            swap(a[lo++], a[mid++]);
+            break; 
+  
+        // If the element is 1 . 
+        case 1: 
+            mid++; 
+            break; 
+  
+        // If the element is 2 
+        case 2: 
+            swap(a[mid], a[hi--]); 
+            break; 
+        } 
+    } 
+    
+    //print array
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+int main() {
+	//code
+	int t,n;
+	cin>>t;
+	
+	while(t--){
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++) cin>>a[i];
+	    sort012(a,n);
+	}
+	return 0;
+}
